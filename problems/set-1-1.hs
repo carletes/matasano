7d75bddd47c82494221d3aacc363bb2265d97d87
Carlos Valiente <carlos@pepelabs.net>
2013-08-11 20:28:09 +0100

    Problem 1.1

--- problems/set-1-1.hs
+++ problems/set-1-1.hs
@@ -13,6 +13,20 @@ import qualified Matasano as M
 source = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 dest = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
+result :: String
+result = case M.hexToBytes source of
+           Left err -> "Nope: " ++ err
+           Right bs -> let enc = M.bytesToBase64 bs
+                           bs1 = M.base64ToBytes enc in
+                       if enc /= dest
+                       then "Nope: " ++ enc ++ " /= " ++ dest
+                       else
+                           case bs1 of
+                             Left err  -> "Nope: " ++ err
+                             Right bs2 -> let enc1 = M.bytesToHex bs2 in
+                                          if enc1 == source
+                                          then "OK"
+                                          else "Nope: " ++ enc1 ++ " /= " ++ source
+
 main = do
-  putStrLn "Not yet"
-              
+  putStrLn result
--- src/Matasano.hs
+++ src/Matasano.hs
@@ -9,20 +9,35 @@ module Matasano
     , bytesToBase64
     ) where
 
+-- Looks like the best way to handle raw byte data in Haskell is with
+-- the 'Data.ByteString' module.
+--
+-- I'll use the lazy version here, since I might need this for data that
+-- does not fit into memory.
 import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Base16.Lazy as B16
+import qualified Data.ByteString.Base64.Lazy as B64
 
 -- | Return a byte string from its hex string representaion
-hexToBytes    :: String -> B.ByteString
-hexToBytes = error "XXX Unimplemented"
+hexToBytes :: String -> Either String B.ByteString
+hexToBytes hs = let (decoded, error) = B16.decode $ C.pack hs
+                    errorStr = show error in
+                case errorStr of
+                  "\"\"" -> Right decoded
+                  otherwise -> Left $ "Invalid hex string: " ++ errorStr
 
 -- | Return the hex representation of a byte string
-bytesToHex    :: B.ByteString -> String
-bytesToHex = error "XXX Unimplemented"
+bytesToHex :: B.ByteString -> String
+bytesToHex =  C.unpack . B16.encode
 
 -- | Return a byte string from its base-64 representation
-base64ToBytes    :: String -> B.ByteString
-base64ToBytes = error "XXX Unimplemented"
+base64ToBytes    :: String -> Either String B.ByteString
+base64ToBytes bs = let bs' = B64.decode $ C.pack bs in
+                   case bs' of
+                     Left err -> Left $ "Invalid bas64 string: " ++ err
+                     Right _  -> bs'
 
 -- | Return the base-64 representation of a byte string
 bytesToBase64 :: B.ByteString -> String
-bytesToBase64 = error "XXX Unimplemented"
+bytesToBase64 = C.unpack . B64.encode
