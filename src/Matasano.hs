6306588cc885f542e5d8bb18b7fc6204111293c0
Carlos Valiente <carlos@pepelabs.net>
2013-08-19 20:54:12 +0100

    Prob;em 1.3: First steps

--- src/Matasano.hs
+++ src/Matasano.hs
@@ -11,6 +11,8 @@ module Matasano
     , bytesToASCII
       -- * Encryption functions
     , xorEncrypt
+      -- * Text analysis functions
+    , frequencies
     ) where
 
 -- Looks like the best way to handle raw byte data in Haskell is with
@@ -22,6 +24,7 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.ByteString.Base16.Lazy as B16
 import qualified Data.ByteString.Base64.Lazy as B64
+import qualified Data.Map as Map
 
 -- For 'xor'
 import Data.Bits
@@ -55,3 +58,11 @@ bytesToASCII = C.unpack
 -- | Encrypts a byte string by xor'ing with another one
 xorEncrypt     :: B.ByteString -> B.ByteString -> B.ByteString
 xorEncrypt a b = B.pack $ zipWith (xor) (B.unpack a) (B.unpack b)
+
+-- | Return a 'Data.Map' of the frequences of each byte in a byte string
+frequencies    :: B.ByteString -> Map.Map Char Integer
+frequencies bs =  foldl process Map.empty (C.unpack bs) where
+    process :: Map.Map Char Integer -> Char -> Map.Map Char Integer
+    process map c = case Map.lookup c map of
+                      Nothing -> Map.insert c 1 map
+                      Just n  -> Map.insert c (n + 1) map
