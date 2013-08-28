f20822104cbb7ccc4c4431b0288bebf3c2e0f67f
Carlos Valiente <carlos@pepelabs.net>
2013-08-28 20:51:59 +0100

    Placeholder for problem 1.5

---
+++ problems/problem-1-5.hs
@@ -0,0 +1,18 @@
+--- 5. Repeating-key XOR Cipher
+---
+--- Write the code to encrypt the string:
+---
+---   Burning 'em, if you ain't quick and nimble
+---   I go crazy when I hear a cymbal
+---
+--- Under the key "ICE", using repeating-key XOR. It should come out to:
+---
+---   0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a2622632427276527
+---   2a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+---
+--- Encrypt a bunch of stuff using your repeating-key XOR function. Get a
+--- feel for it.
+
+main :: IO ()
+main = do
+  putStrLn "Not yet"
