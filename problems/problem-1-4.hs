918870c068e1d441c22ee91f05205293e778106c
Carlos Valiente <carlos@pepelabs.net>
2013-08-28 20:03:25 +0100

    Problem 1.4 solved

--- problems/problem-1-4.hs
+++ problems/problem-1-4.hs
@@ -10,25 +10,60 @@
 import Data.Word
 import System.Environment
 import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Lazy.Char8 as C
 
 import qualified Matasano as M
 
-getStrings       :: FilePath -> IO [B.ByteString]
+getStrings       :: FilePath -> IO [Either String B.ByteString]
 getStrings fname = do
-  body <- B.readFile fname
-  return (C.split '\n' body)
+  body <- readFile fname
+  return (map M.hexToBytes (lines body))
 
-decryptedStrings      :: B.ByteString -> M.Frequencies -> [(Double, Word8, B.ByteString)]
-decryptedStrings bs f = map process keys where
-    keys = [1 .. 255]
-    process :: Word8 -> (Double, Word8, B.ByteString)
-    process k = (r, k, decrypted) where
-        decrypted = M.xorEncrypt bs (B.pack [k])
-        r         = M.rank decrypted f
+data RankedKey = RankedKey {
+      key       :: Word8,
+      rank      :: Double,
+      decrypted :: B.ByteString
+}
 
+data Candidate = Candidate {
+      original :: B.ByteString,
+      keys     :: [RankedKey]
+}
+
+candidates :: [Either String B.ByteString] -> M.Frequencies -> [Either String Candidate]
+candidates bbs f = map process bbs where
+    process    :: Either String B.ByteString -> Either String Candidate
+    process bs = case bs of
+                   Left err  -> Left err
+                   Right val -> Right (Candidate val (decryptRank val f))
+
+decryptRank      :: B.ByteString -> M.Frequencies -> [RankedKey]
+decryptRank bs f = map process ks where
+    ks = [1 .. 255]
+    process   :: Word8 -> RankedKey
+    process k = RankedKey k r d where
+        r   = M.rank d f
+        d   = M.xorEncrypt bs (B.replicate len k)
+        len = B.length bs
+
+printCandidate              :: Double -> Either String Candidate -> IO ()
+printCandidate _ (Left err) = putStrLn err
+printCandidate r (Right c)  = do
+  putStrLn $ M.bytesToHex (original c)
+  mapM_ printKey ks where
+      printKey   :: RankedKey -> IO ()
+      printKey k = putStrLn $ "\t" ++ show (rank k) ++ " " ++ show (key k) ++ " " ++ show (decrypted k)
+      ks         = filter (\k -> (rank k) < r) (keys c)
+
+bellowRank             :: Double -> Either String Candidate -> Bool
+bellowRank _ (Left _)  = False
+bellowRank r (Right c) = any (\k -> (rank k) < r) (keys c)
+
+main :: IO ()
 main = do
   [dataFile, corpusFile] <- getArgs
   freqs <- M.corpusFrequencies corpusFile
-  result <- getStrings dataFile
-  putStrLn $ show result
+  input <- getStrings dataFile
+  let allCandidates = candidates input freqs
+      threshold     = 0.5
+      probable      = filter (bellowRank threshold) allCandidates
+  mapM_ (printCandidate threshold) probable
