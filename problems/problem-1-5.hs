9f292069f705668002fc30784e69ed9014a76c2c
Carlos Valiente <carlos.valiente@ecmwf.int>
2013-08-30 13:32:22 +0100

    Problem 1.5 solved

--- problems/problem-1-5.hs
+++ problems/problem-1-5.hs
@@ -13,6 +13,33 @@
 --- Encrypt a bunch of stuff using your repeating-key XOR function. Get a
 --- feel for it.
 
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
+
+import qualified Matasano as M
+
+input :: String
+input = "Burning 'em, if you ain't quick and nimble\n" ++
+        "I go crazy when I hear a cymbal"
+
+key :: String
+key = "ICE"
+
+expected :: String
+expected = "0b3637272a2b2e63622c2e69692a23693a2a3" ++
+           "c6324202d623d63343c2a2622632427276527" ++
+           "2a282b2f20430a652e2c652a3124333a653e2" ++
+           "b2027630c692b20283165286326302e27282f"
+
+result :: String
+result = M.bytesToHex $ M.xorEncrypt (C.pack input) key' where
+    key' :: B.ByteString
+    key' = (C.pack . concat . repeat) key
+
 main :: IO ()
-main = do
-  putStrLn "Not yet"
+main =
+    if expected == result
+    then do
+      putStrLn "OK"
+    else do
+      putStrLn $ "Error: " ++ result ++ " /= " ++ expected
