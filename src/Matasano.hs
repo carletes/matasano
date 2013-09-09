8e6e656b92d519d898392683f838095914ad8535
Carlos Valiente <carlos@pepelabs.net>
2013-09-09 18:53:43 +0100

    Haddock fixes

--- src/Matasano.hs
+++ src/Matasano.hs
@@ -4,21 +4,26 @@ module Matasano
     ( -- * Hexadecimal conversion functions
       hexToBytes
     , bytesToHex
+
       -- * Base-64 conversion functions
     , base64ToBytes
     , bytesToBase64
+
       -- * ASCII conversion functions
     , bytesToASCII
+
       -- * Encryption functions
     , xorEncrypt
+
       -- * Text analysis functions
     , Frequencies
     , corpusFrequencies
     , frequencies
     , rank
     , hammingDistance
+
       -- * Cryptanalysis functions
-    , RankedKey(..)
+    , RankedKey
     , guessXorKey
     ) where
 
@@ -31,12 +36,11 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.ByteString.Base16.Lazy as B16
 import qualified Data.ByteString.Base64.Lazy as B64
-
 import qualified Data.Map as Map
 import Data.Bits (popCount, xor)
-import Data.Word
+import Data.Word (Word8)
 
--- | Return a byte string from its hex string representaion
+-- | Return a byte string from its hex string representation.
 hexToBytes :: String -> Either String B.ByteString
 hexToBytes hs = let (decoded, err) = B16.decode $ C.pack hs
                     errorStr = C.unpack err in
@@ -44,35 +48,36 @@ hexToBytes hs = let (decoded, err) = B16.decode $ C.pack hs
                   "" -> Right decoded
                   _  -> Left $ "Invalid hex string: " ++ errorStr
 
--- | Return the hex representation of a byte string
+-- | Return the hex representation of a byte string.
 bytesToHex :: B.ByteString -> String
 bytesToHex =  C.unpack . B16.encode
 
--- | Return a byte string from its base-64 representation
+-- | Return a byte string from its base-64 representation.
 base64ToBytes    :: String -> Either String B.ByteString
 base64ToBytes bs = let bs' = B64.decode $ C.pack bs in
                    case bs' of
                      Left err -> Left $ "Invalid base64 string: " ++ err
                      Right _  -> bs'
 
--- | Return the base-64 representation of a byte string
+-- | Return the base-64 representation of a byte string.
 bytesToBase64 :: B.ByteString -> String
 bytesToBase64 = C.unpack . B64.encode
 
--- | Return the ASCII encoding of a byte string
+-- | Return the ASCII encoding of a byte string.
 bytesToASCII :: B.ByteString -> String
 bytesToASCII = C.unpack
 
--- | Encrypts a byte string by xor'ing with another one
+-- | Encrypts a byte string by xor'ing with another one.
 xorEncrypt     :: B.ByteString -> B.ByteString -> B.ByteString
 xorEncrypt a b = B.pack $ zipWith (xor) (B.unpack a) (B.unpack b)
 
-data Frequencies = Freqs {
-      freqs :: Map.Map Char Integer,
-      norm  :: Double
-}
+-- | A structure describing the frequencies of bytes in a byte string.
+data Frequencies = Freqs
+    { freqs :: Map.Map Char Integer  -- ^ The frequency map
+    , norm  :: Double                -- ^ Length of the byte string
+    }
 
--- | Return the frequency map of each byte in a byte string
+-- | Return the frequency map of each byte in a byte string.
 frequencies    :: B.ByteString -> Frequencies
 frequencies bs =  Freqs freqMap norm' where
     freqMap = foldl process Map.empty (C.unpack bs)
@@ -82,7 +87,7 @@ frequencies bs =  Freqs freqMap norm' where
                     Nothing -> Map.insert c 1 m
                     Just n  -> Map.insert c (n + 1) m
 
--- | Return the frequency of a given byte in a frequency map
+-- | Return the frequency of a given byte in a frequency map.
 frequency     :: Char -> Frequencies -> Double
 frequency c f = case Map.lookup c (freqs f) of
                   Nothing -> 0.0
@@ -96,20 +101,25 @@ rank bs f = sum $ map freqDelta bsChars where
                 freqBs      = frequencies bs
                 bsChars     = Map.keys (freqs freqBs)
 
--- | Return the character frequencies of a given corpus file
+-- | Return the character frequencies of a given corpus file.
 corpusFrequencies       :: FilePath -> IO Frequencies
 corpusFrequencies fname =  do
   corpus <- B.readFile fname
   return (frequencies corpus)
 
-data RankedKey = RankedKey {
-      key       :: Word8,
-      rnk       :: Double,
-      decrypted :: B.ByteString
+-- | A key ranked according to some criteria.
+data RankedKey = RankedKey
+    { key       :: Word8         -- ^ The key
+    , rnk       :: Double        -- ^ The rank of this key
+    , decrypted :: B.ByteString  -- ^ The byte string decrypted with this key
 } deriving (Show)
 
 -- | Guess the single-byte XOR key used to encrypt the given byte string.
-guessXorKey :: B.ByteString -> Frequencies -> Double -> [RankedKey]
+--
+-- Given a byte string, a frequencies map and a threshold, returns a list of
+-- ranked keys whose rank (for the given frequencies map) is bellow the given
+-- threshold
+guessXorKey        :: B.ByteString -> Frequencies -> Double -> [RankedKey]
 guessXorKey bs f t = filter (\k -> rnk k < t) candidates where
     candidates = map go [1 .. 225]
     go   :: Word8 -> RankedKey
@@ -118,6 +128,6 @@ guessXorKey bs f t = filter (\k -> rnk k < t) candidates where
         d   = xorEncrypt bs (B.replicate len k)
         len = B.length bs
 
--- | Return the number of differing bits in two byte strings
+-- | Return the number of differing bits in two byte strings.
 hammingDistance :: B.ByteString -> B.ByteString -> Int
 hammingDistance bs cs = sum $ map (\(a, b) -> popCount $ xor a b) (B.zip bs cs)
