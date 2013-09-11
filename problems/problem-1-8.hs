05dbbe3eb34d525c76b462b2860831ab3d3385b7
Carlos Valiente <carlos@pepelabs.net>
2013-09-11 21:50:15 +0100

    Work on problem 1.8

--- problems/problem-1-8.hs
+++ problems/problem-1-8.hs
@@ -10,6 +10,39 @@
 -- deterministic; the same 16 byte plaintext block will always produce
 -- the same 16 byte ciphertext.
 
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(..))
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Map as Map
+
+import qualified Matasano as M
+import Matasano.Utils (usage)
+
+getStrings :: String -> Either String [B.ByteString]
+getStrings body = sequence $ take 1000 $ map M.hexToBytes (lines body)
+
+type ChunkFrequencies = Map.Map B.ByteString Integer
+
+chunkFreq      :: Integer -> B.ByteString -> ChunkFrequencies
+chunkFreq n bs = foldl go Map.empty (M.chunks n bs) where
+    go :: ChunkFrequencies -> B.ByteString -> ChunkFrequencies
+    go freq chunk = case Map.lookup chunk freq of
+                      Nothing -> Map.insert chunk 1 freq
+                      Just n  -> Map.insert chunk (n + 1) freq
+
 main :: IO ()
 main = do
-  putStrLn "Not yet"
+  argv <- getArgs
+  case argv of
+    [dataFile] -> do
+              body <- readFile dataFile
+              let input = getStrings body
+              case input of
+                Left err -> do
+                           putStrLn $ "Malformed input: " ++ err
+                           exitWith $ ExitFailure 1
+                Right input' -> do
+                           mapM_ (putStrLn . show . chunkFreq 4) input'
+
+    _ -> usage "<data file>"
