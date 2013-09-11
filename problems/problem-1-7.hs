3d3f1088dd9bbedca0ca43b5d7a00d45cb76b74f
Carlos Valiente <carlos@pepelabs.net>
2013-09-11 19:36:39 +0100

    Problem 1.7 solved

---
+++ problems/problem-1-7.hs
@@ -0,0 +1,50 @@
+-- 7. AES in ECB Mode
+--
+-- The Base64-encoded content at the following location:
+--
+--     https://gist.github.com/3132853
+--
+-- Has been encrypted via AES-128 in ECB mode under the key
+--
+--    "YELLOW SUBMARINE".
+--
+-- (I like "YELLOW SUBMARINE" because it's exactly 16 bytes long).
+--
+-- Decrypt it.
+--
+-- Easiest way:
+--
+-- Use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+
+import Crypto.Cipher.AES (decryptECB, encryptECB, initAES)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(..))
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as C
+
+import qualified Matasano as M
+import Matasano.Utils
+
+main :: IO ()
+main = do
+  argv <- getArgs
+  case argv of
+    [dataFile] -> do
+              input <- readFile dataFile
+              case ((M.base64ToBytes . concat . lines) input) of
+                Left err -> do
+                            putStrLn $ "Malformed input: " ++ err
+                            exitWith $ ExitFailure 1
+                Right input' -> do
+                    let input2 = B.toStrict input'
+                        key = initAES $ C.pack "YELLOW SUBMARINE"
+                        dec = decryptECB key input2
+                        enc = encryptECB key dec
+                    if enc /= input2
+                       then do
+                         putStrLn $ "Error: encrypt(decrypt(input)) /= input"
+                         exitWith $ ExitFailure 1
+                      else
+                          putStrLn $ C.unpack dec
+    _ -> usage "<data file>"
